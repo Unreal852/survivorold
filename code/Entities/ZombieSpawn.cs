750def2508032be119bf76914bf86d28910bcdc3
Unreal852 <unreal852@hotmail.fr>
2022-08-29 14:01:29 +0200

    ZombieSpawnPoint
    A ZombieSpawnPoint class that should be usable in Hammer

---
+++ code/Entities/ZombieSpawn.cs
@@ -0,0 +1,10 @@
+﻿using SandboxEditor;
+
+namespace Sandbox.Entities;
+
+[Library( "survivor_zombie_spawn" )]
+[HammerEntity, EditorModel( "models/editor/playerstart.vmdl", FixedBounds = true )]
+[Title( "Zombie Spawnpoint" ), Category( "Zombie" ), Icon( "place" )]
+public class ZombieSpawn : SpawnPoint
+{
+}
