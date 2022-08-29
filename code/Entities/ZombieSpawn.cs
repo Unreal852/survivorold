ec38fb5f4ae7aab2ffcc45ce8ed48cfad7a996c7
Unreal852 <unreal852@hotmail.fr>
2022-08-29 22:05:18 +0200

    Update ZombieSpawn.cs

--- code/Entities/ZombieSpawn.cs
+++ code/Entities/ZombieSpawn.cs
@@ -6,8 +6,7 @@ namespace Survivor.Entities;
 [Library( "survivor_zombie_spawn" )]
 [HammerEntity, EditorModel( "models/editor/playerstart.vmdl", FixedBounds = true )]
 [Title( "Zombie Spawnpoint" ), Category( "Zombie" ), Icon( "place" )]
-public class ZombieSpawn : SpawnPoint
+public partial class ZombieSpawn : SpawnPoint
 {
-	[Property( Title = "Model" ) ]
-	public Model ModelToSpawn { get; set; }
+	[Property( Title = "Model" )] public Model ModelToSpawn { get; set; }
 }
