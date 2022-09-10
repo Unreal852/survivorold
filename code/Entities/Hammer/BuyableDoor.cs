d6ae75f496ce716d7bbe256a23962d7af0259096
Unreal852 <unreal852@hotmail.fr>
2022-09-10 22:01:01 +0200

    Update BuyableDoor.cs

--- code/Entities/Hammer/BuyableDoor.cs
+++ code/Entities/Hammer/BuyableDoor.cs
@@ -15,21 +15,34 @@ namespace Survivor.Entities.Hammer;
 public partial class BuyableDoor : ModelEntity, IUse
 {
 	// TODO: Door open direction
-	
+
 	private bool      _bought = false;
 	private TimeSince _timeSinceBought;
 
 	[Property]
-	[Title( "Enabled" ), Description( "Unchecking this will prevent this door from being bought" )]
+	[Category( "Door" ), Title( "Enabled" ), Description( "Unchecking this will prevent this door from being bought" )]
 	public bool IsEnabled { get; set; } = true;
 
 	[Property]
-	[Title( "Cost" ), Description( "The cost to unlock this door" )]
+	[Category( "Door" ), Title( "Cost" ), Description( "The cost to unlock this door" )]
 	public int Cost { get; set; } = 0;
 
 	[Property, FGDType( "target_destination" )]
-	[Title( "Room" ), Description( "The room which this door will unlock. Multiple doors can unlock a single room" )]
+	[Category( "Door" ), Title( "Room" ), Description( "The room which this door will unlock. Multiple doors can unlock a single room" )]
 	public string Room { get; set; } = "";
+
+	[Property]
+	[Category( "Door" ), Title( "Open Direction" ),
+	 Description( "The direction where the door goes to play its opening animation. (Setting 0 will disable animation and the door will be instantly deleted" )]
+	public Vector3 OpenDirection { get; set; } = Vector3.Down;
+
+	[Property]
+	[Category( "Door" ), Title( "Open Speed" ), Description( "The open animation speed" )]
+	public float OpenSpeed { get; set; } = 500f;
+
+	[Property]
+	[Category( "Door" ), Title( "Delete after" ), Description( "Delete this door after the set amount of time (in seconds)" )]
+	public float DeleteAfter { get; set; } = 2.5f;
 
 	public override void Spawn()
 	{
@@ -75,9 +88,11 @@ public partial class BuyableDoor : ModelEntity, IUse
 				player.Money -= Cost;
 				if ( Owner is Room room )
 					room.IsBought = true;
-				ChatBox.AddChatEntry( To.Everyone, "Survivor", $"{player.Client.Name} opened {Room} for {Cost}$" );
 				_bought = true;
 				_timeSinceBought = 0;
+				ChatBox.AddChatEntry( To.Everyone, "Survivor", $"{player.Client.Name} opened {Room} for {Cost}$" );
+				if ( OpenDirection == Vector3.Zero )
+					Delete();
 				return true;
 			}
 		}
@@ -95,11 +110,9 @@ public partial class BuyableDoor : ModelEntity, IUse
 	{
 		if ( IsValid && _bought )
 		{
-			Position += Vector3.Down * 500 * Time.Delta;
-			if ( _timeSinceBought >= 3 )
-			{
+			Position += OpenDirection * OpenSpeed * Time.Delta;
+			if ( _timeSinceBought >= DeleteAfter )
 				Delete();
-			}
 		}
 	}
 }
