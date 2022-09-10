183bc1e7bec8c71f468af08c417e07f41354a6b8
Unreal852 <unreal852@hotmail.fr>
2022-09-10 21:45:08 +0200

    Update SurvivorPlayer.Use.cs

--- code/Players/SurvivorPlayer.Use.cs
+++ code/Players/SurvivorPlayer.Use.cs
@@ -13,7 +13,7 @@ public partial class SurvivorPlayer
 	{
 		if ( !Host.IsClient )
 			return;
-		Using = FindUsableWithTraceResult( out var tr );
+		Using = FindUsable( );
 		if ( Using != null && _interactableIndicator == null )
 		{
 			_interactableIndicator = new InteractableIndicator( this );
@@ -23,42 +23,5 @@ public partial class SurvivorPlayer
 			_interactableIndicator?.Delete();
 			_interactableIndicator = null;
 		}
-	}
-
-	protected virtual Entity FindUsableWithTraceResult( out TraceResult traceResult )
-	{
-		// First try a direct 0 width line
-		var tr = Trace.Ray( EyePosition, EyePosition + EyeRotation.Forward * 85 )
-		              .Ignore( this )
-		              .Run();
-		traceResult = tr;
-
-		// See if any of the parent entities are usable if we ain't.
-		var ent = tr.Entity;
-		while ( ent.IsValid() && !IsValidUseEntity( ent ) )
-		{
-			ent = ent.Parent;
-		}
-
-		// Nothing found, try a wider search
-		if ( !IsValidUseEntity( ent ) )
-		{
-			tr = Trace.Ray( EyePosition, EyePosition + EyeRotation.Forward * 85 )
-			          .Radius( 2 )
-			          .Ignore( this )
-			          .Run();
-
-			// See if any of the parent entities are usable if we ain't.
-			ent = tr.Entity;
-			while ( ent.IsValid() && !IsValidUseEntity( ent ) )
-			{
-				ent = ent.Parent;
-			}
-		}
-
-		// Still no good? Bail.
-		if ( !IsValidUseEntity( ent ) ) return null;
-
-		return ent;
 	}
 }
