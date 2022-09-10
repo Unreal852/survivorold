b64bdeef6fbcea4a0093c5811e00a0a81cda473c
Unreal852 <unreal852@hotmail.fr>
2022-09-10 21:48:52 +0200

    Create WeaponStand.cs

---
+++ code/Entities/Hammer/WeaponStand.cs
@@ -0,0 +1,27 @@
+﻿using Sandbox;
+using SandboxEditor;
+
+// resharper disable all
+
+namespace Survivor.Entities.Hammer;
+
+[Library( "survivor_weapon_stand" )]
+[Title( "Weapon Stand" ), Category( "Map" ), Icon( "place" ), Description( "This entity defines weapon stand" )]
+[HammerEntity, SupportsSolid, Model( Model = "models/objects/distributeur.vmdl", Archetypes = ModelArchetype.generic_actor_model )]
+[RenderFields, VisGroup( VisGroup.Dynamic )]
+public partial class WeaponStand : ModelEntity
+{
+	[Property]
+	[Title( "Enabled" ), Description( "Unchecking this will prevent this weapon from being bought" )]
+	public bool IsEnabled { get; set; } = true;
+
+	[Property]
+	[Title( "Cost" ), Description( "The cost to buy this weapon" )]
+	public int Cost { get; set; } = 0;
+
+	public override void Spawn()
+	{
+		base.Spawn();
+		SetupPhysicsFromModel( PhysicsMotionType.Static );
+	}
+}
