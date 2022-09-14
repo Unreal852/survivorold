58bbb515b93c734e314bda3d92645713ca81788e
Unreal852 <unreal852@hotmail.fr>
2022-09-14 14:27:46 +0200

    Update ZombieData.cs

--- code/GameResources/ZombieData.cs
+++ code/GameResources/ZombieData.cs
@@ -6,7 +6,12 @@ namespace Survivor.GameResources;
 [GameResource( "Zombie Data", "zombie", "Describes a zombie" )]
 public class ZombieData : GameResource
 {
-	[Category( "General" )]                       public string FriendlyName { get; set; }
-	[Category( "Model" ), ResourceType( "vmdl" )] public string Model        { get; set; }
-	[Category( "Behaviour" )]                     public float  MoveSpeed    { get; set; }
+	[Category( "General" )]                       public string FriendlyName  { get; set; }
+	[Category( "Model" ), ResourceType( "vmdl" )] public string Model         { get; set; }
+	[Category( "Model" )]                         public Color  RenderColor   { get; set; }
+	[Category( "Behaviour" )]                     public float  Health        { get; set; }
+	[Category( "Behaviour" )]                     public float  MoveSpeed     { get; set; }
+	[Category( "Behaviour" )]                     public float  AttackRange   { get; set; }
+	[Category( "Behaviour" )]                     public float  AttackDamages { get; set; }
+	[Category( "Behaviour" )]                     public float  AttackSpeed   { get; set; }
 }
