51835c3659198e662ddc3930e391f235a8672428
Unreal852 <unreal852@hotmail.fr>
2022-09-18 17:54:13 +0200

    ViewModeOffSet init changes

--- code/Weapons/AK47.cs
+++ code/Weapons/AK47.cs
@@ -13,8 +13,9 @@ public sealed class AK47 : BaseWeapon
 		Primary.ScreenShake = new ScreenShake { Length = 0.08f, Delay = 0.02f, Size = 1.2f, Rotation = 0.1f }; // TODO: Move this into WeaponAsset
 		RunAnimData = new AngPos { Angle = new Angles( 27.7f, 39.95f, 0f ), Pos = new Vector3( 6.184f, 0f, 8.476f ) };
 		ZoomAnimData = new AngPos { Angle = new Angles( 0f, 0f, 0f ), Pos = new Vector3( -14.635f, -5f, 7.812f ) };
+		ViewModelOffset = new() { Angle = new Angles( 0f, 0f, 0f ), Pos = new Vector3( 0f, -5f, 0f ) };
 	}
 
 	public override HoldType HoldType        { get; } = HoldType.Rifle; // TODO: Move this into WeaponAsset
-	public override AngPos   ViewModelOffset { get; } = new() { Angle = new Angles( 0f, 0f, 0f ), Pos = new Vector3( 0f, -5f, 0f ) };
+	public override AngPos   ViewModelOffset { get; }
 }
--- code/Weapons/Magnum.cs
+++ code/Weapons/Magnum.cs
@@ -12,11 +12,12 @@ public sealed class Magnum : BaseWeapon
 			return;
 		Primary.ScreenShake = new ScreenShake { Length = 0.08f, Delay = 0.02f, Size = 1.9f, Rotation = 0.4f }; // TODO: Move this into WeaponAsset
 
-		RunAnimData = new AngPos { Angle = new Angles( 27.7f, 39.95f, 0f ), Pos = new Vector3( 6.955f, -28.402f, 2.965f ) };
-		DuckAnimData = new AngPos { Angle = new Angles( 5.08f, -2.89f, -25.082f ), Pos = new Vector3( -49.547f, 0f, 2.885f ) };
-		ZoomAnimData = new AngPos { Angle = new Angles( 0f, 0f, 0f ), Pos = new Vector3( -32.063f, -11f, 8.161f ) };
+		RunAnimData = new() { Angle = new Angles( 27.7f, 39.95f, 0f ), Pos = new Vector3( 6.955f, -28.402f, 2.965f ) };
+		DuckAnimData = new() { Angle = new Angles( 5.08f, -2.89f, -25.082f ), Pos = new Vector3( -49.547f, 0f, 2.885f ) };
+		ZoomAnimData = new() { Angle = new Angles( 0f, 0f, 0f ), Pos = new Vector3( -32.063f, -11f, 8.161f ) };
+		ViewModelOffset = new() { Angle = new Angles( 0f, 0f, 0f ), Pos = new Vector3( 0f, -30f, 0f ) };
 	}
 
 	public override HoldType HoldType        { get; } = HoldType.Pistol; // TODO: Move this into WeaponAsset
-	public override AngPos   ViewModelOffset { get; } = new() { Angle = new Angles( 0f, 0f, 0f ), Pos = new Vector3( 0f, -30f, 0f ) };
+	public override AngPos   ViewModelOffset { get; }
 }
