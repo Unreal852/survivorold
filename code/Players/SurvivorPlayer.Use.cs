9c49fa77e9d3959bb85ab9b9ec0c129d306204f6
Unreal852 <unreal852@hotmail.fr>
2022-09-19 19:09:07 +0200

    Update SurvivorPlayer.Use.cs

--- code/Players/SurvivorPlayer.Use.cs
+++ code/Players/SurvivorPlayer.Use.cs
@@ -4,8 +4,6 @@ namespace Survivor.Players;
 
 public sealed partial class SurvivorPlayer
 {
-	// TODO: This is badly written, its only here for testing
-
 	private void TickPlayerUseClient()
 	{
 		if ( !Host.IsClient )
