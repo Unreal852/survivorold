d698843ff9b19101f70f80b4b41eb33a5a467bcc
Unreal852 <unreal852@hotmail.fr>
2022-09-20 18:50:33 +0200

    Small position changes

--- code/Weapons/FN57.cs
+++ code/Weapons/FN57.cs
@@ -17,37 +17,29 @@ public sealed class FN57 : ABaseWeapon
 		ZoomAnimData = new AngPos { Angle = new Angles( 0.46f, -0.14f, 0f ), Pos = new Vector3( -3.928f, -4.795f, 0.745f ) };
 		ViewModelOffset = new AngPos { Angle = new Angles( 0f, 0f, 0f ), Pos = new Vector3( 0f, -10.56f, -2.4f ) };
 
-
-		AttachmentCategories = new List<AttachmentCategory>
+		AttachmentCategories = new();
+		AttachmentCategories.Add( new()
 		{
-				new()
+				Name = AttachmentCategoryName.Sight,
+				BoneOrAttachment = "sight",
+				Attachments = new List<AttachmentBase>
 				{
-						Name = AttachmentCategoryName.Sight,
-						BoneOrAttachment = "sight",
-						Attachments = new List<AttachmentBase>
+						new RedDot
 						{
-								new RedDot
+								ZoomAnimData = new AngPos { Angle = new Angles( 0.72f, -0.12f, 0f ), Pos = new Vector3( -3.921f, -4.795f, 0.544f ) },
+								ViewParentBone = "sight",
+								ViewTransform =
+										new Transform { Position = new Vector3( 0, 0, 0 ), Rotation = Rotation.From( new Angles( 0.05f, -0.49f, 0f ) ), Scale = 1f },
+								WorldParentBone = "sight",
+								WorldTransform = new Transform
 								{
-										ZoomAnimData = new AngPos { Angle = new Angles( 0.72f, -0.12f, 0f ), Pos = new Vector3( -3.907f, -4.795f, 0.537f ) },
-										ViewParentBone = "sight",
-										ViewTransform =
-												new Transform
-												{
-														Position = new Vector3( 0, 0, 0 ),
-														Rotation = Rotation.From( new Angles( 0.05f, -0.49f, 0f ) ),
-														Scale = 1f
-												},
-										WorldParentBone = "sight",
-										WorldTransform = new Transform
-										{
-												Position = new Vector3( 0, 0, 0 ),
-												//Rotation = Rotation.From( new Angles( -90f, 0f, -90f ) ),
-												Scale = 4f
-										}
+										Position = new Vector3( 0, 0, 0 ),
+										//Rotation = Rotation.From( new Angles( -90f, 0f, -90f ) ),
+										Scale = 4f
 								}
 						}
 				}
-		};
+		} );
 	}
 
 	public override string   ViewModelPath   => "models/weapons/pistols/fn57/vm_fn57.vmdl";
@@ -59,6 +51,6 @@ public sealed class FN57 : ABaseWeapon
 public class RedDot : Sight
 {
 	public override string Name      => "Reflex";
-	public override string IconPath  => "attachments/swb/sight/reflex/ui/icon.png";
+	public override string IconPath  => "";
 	public override string ModelPath => "models/attachments/red_dot.vmdl";
 }
