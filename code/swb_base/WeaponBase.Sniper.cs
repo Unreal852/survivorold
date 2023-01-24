63fab3a03fe0f45616e750d6c5375b136368b2f9
Unreal852 <unreal852@hotmail.fr>
2023-01-24 21:43:29 +0100

    SWP Update

--- code/swb_base/WeaponBase.Sniper.cs
+++ code/swb_base/WeaponBase.Sniper.cs
@@ -25,7 +25,7 @@ public partial class WeaponBaseSniper : WeaponBase
     /// <summary>EXPERIMENTAL - Use a render target instead of a full screen texture zoom</summary>
     public virtual bool UseRenderTarget => false;
 
-    private Panel SniperScopePanel;
+    protected Panel SniperScopePanel;
     private bool switchBackToThirdP = false;
     private float oldSpread = -1;
 
@@ -122,7 +122,7 @@ public partial class WeaponBaseSniper : WeaponBase
             OnScopedStart();
         }
 
-        if (Input.Released(InputButton.SecondaryAttack) || (IsScoped && (IsRunning || shouldTuck)))
+        if (Input.Released(InputButton.SecondaryAttack) || (IsScoped && (IsRunning || shouldTuck || IsReloading)))
         {
             OnScopedEnd();
         }
